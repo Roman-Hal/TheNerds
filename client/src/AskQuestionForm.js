95d67ec5a25bd62e9ee82f18ddd2167978add9ac
Krishan <Krishan-Software-developer@outlook.com>
2022-03-12 14:50:51 +0000

    Ask Questions
    still working on it

--- client/src/AskQuestionForm.js
+++ client/src/AskQuestionForm.js
@@ -3,78 +3,25 @@ import React from 'react';
 
 const Form=() => {
 
-
-
-// class Form extends React.Component {
-//     state = {
-//         firstName: '',
-//         lastName: '',
-//         username: '',
-//         email: '',
-//         password: '',
-
-//     };
-//        change = e => {
-//             this.setState({
-//               [e.target.name]: e.target.value
-//             });
-//           };
-
-//       onSubmit = e => {
-//         e.preventDefault();
-//         console.log(this.state);
-//       };
-           
-//         // function to update state of name with
-//         // value enter by user in form
-
-//         // function to update state of email with value
-//         //function to update state of password with
-
-       
-
-//       render() {    
-        
        return (
           <form>
             <input 
-            name="firstName"
-            placeholder="First name"
-            value={this.state.firstName} 
+            name="Title"
+            placeholder="Title"
              onChange={e => this.change(e)}
             />
             <br />
-              <input
-               name="lastName"
-               placeholder="Last name"
-               value={this.state.lastName} 
-               onChange={e => this.change(e)}
-             />
+              <textarea> Description </textarea>
+             
              <br />
                 <input 
                 name="username"
-                placeholder="Username"
-                value={this.state.username} 
+                placeholder="Username" 
                 onChange={e => this.change(e)}
              />
-             <br />     
-                <input
-                 name="email"
-                 placeholder="Email"
-                 value={this.state.email} 
-                 onChange={e => this.change(e)}
-             />
-             <br/>
-              <input 
-               name="password"
-               type="password"
-               placeholder="Password"
-               value={this.state.password} 
-               onChange={e => this.change(e)}
-             />
-              <br />
-
-              <button onClick={e => this.onSubmit(e)}>Submit</button>
+            
+              
+            <button className="button1"  onClick={e => this.onSubmit(e)}>Ask Question </button>
 
           </form>
         );
@@ -82,7 +29,5 @@ const Form=() => {
 }
 
 
-  
-       
-  export default Form;
+   export default Form;
     
